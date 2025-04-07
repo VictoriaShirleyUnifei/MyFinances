1a7d8f6ef2cae67bf9ec8c81dc7da62d098d5a5b
Victoria Shirley Avelino da Silva <victoriashirley@Mac-mini-de-Victoria.local>
2025-04-07 15:52:23 -0300

    feature: BottomNavigationBar

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:my_finances/features/sign_up/sign_up_page.dart';
+import 'package:my_finances/features/onboarding/onboarding_page.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -7,7 +7,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SignUpPage()
+      home: OnboardingPage()
     );
   }
 }
---
+++ lib/features/home/home_page.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:my_finances/common/constants/app_colors.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  final List<Widget> _screens = const [
+    Center(child: Text('Home')),
+    Center(child: Text('Statistics')),
+    Center(child: Text('Wallet')),
+    Center(child: Text('Profile')),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() => _selectedIndex = index);
+  }
+
+  Widget _buildNavItem(IconData icon, int index) {
+    return IconButton(
+      icon: Icon(
+        icon,
+        size: 30,
+        color: _selectedIndex == index ? AppColors.primary : Colors.grey,
+      ),
+      onPressed: () => _onItemTapped(index),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _screens[_selectedIndex],
+
+     floatingActionButton: FloatingActionButton(
+     onPressed: () {
+        showModalBottomSheet(
+          context: context,
+          builder: (context) => const Padding(
+            padding: EdgeInsets.all(20),
+            child: Text('Ação do botão +'),
+          ),
+        );
+      },
+      backgroundColor: AppColors.primary,
+      shape: const CircleBorder(), // deixa o botão 100% redondo
+      child: const Icon(Icons.add, color: Colors.white), 
+    ),
+
+
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        notchMargin: 6,
+        color: Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _buildNavItem(Icons.home, 0),
+              _buildNavItem(Icons.bar_chart, 1),
+              const SizedBox(width: 48), // espaço para o botão flutuante
+              _buildNavItem(Icons.account_balance_wallet_outlined, 2),
+              _buildNavItem(Icons.person_outline, 3),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

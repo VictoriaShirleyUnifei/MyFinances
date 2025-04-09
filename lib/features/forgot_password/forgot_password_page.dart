bd3f2cabc1537a275998b521111b7c9954550949
Victoria Shirley Avelino da Silva <victoriashirley@Mac-mini-de-Victoria.local>
2025-04-09 17:55:56 -0300

    feature: Forgot Password

---
+++ lib/features/forgot_password/forgot_password_page.dart
@@ -0,0 +1,134 @@
+import 'package:flutter/material.dart';
+import 'package:my_finances/common/constants/app_colors.dart';
+import 'package:my_finances/common/constants/app_text_styles.dart';
+import 'package:my_finances/common/widgets/custom_input.dart';
+import 'package:my_finances/common/widgets/primary_button.dart';
+import 'package:reactive_forms/reactive_forms.dart';
+import 'package:logger/logger.dart';
+
+class ForgotPasswordPage extends StatefulWidget {
+  const ForgotPasswordPage({super.key});
+
+  @override
+  ForgotPasswordPageState createState() => ForgotPasswordPageState();
+}
+
+class ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  final logger = Logger();
+
+  final form = FormGroup({
+    'email': FormControl<String>(
+      validators: [Validators.required, Validators.email],
+    ),
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: ReactiveForm(
+        formGroup: form,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Column(
+            children: [
+              const SizedBox(height: 60),
+              Expanded(
+                flex: 2,
+                child: Center(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        'Reset your Password',
+                        textAlign: TextAlign.center,
+                        style: AppTextStyles.bigText.copyWith(
+                          color: AppColors.primary,
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+                      Image.asset('assets/images/forgot_password.png'),
+                      const SizedBox(height: 20),
+
+                      Text(
+                        'Enter your email address and a link will be sent to reset your password.',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16, 
+                          color: AppColors.textSecondary
+                        ),
+                      ),
+                      const SizedBox(height: 30),
+
+
+                      // EMAIL
+                      CustomReactiveTextField(
+                        formControlName: 'email',
+                        label: 'Your Email',
+                        keyboardType: TextInputType.emailAddress,
+                        validationMessages: {
+                          ValidationMessage.required: (_) =>
+                              'Email is required',
+                          ValidationMessage.email: (_) => 'Invalid email',
+                        },
+                      ),
+                      const SizedBox(height: 20),
+
+                      // BUTTON
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 40.0),
+                        child: SizedBox(
+                          width: double.infinity,
+                          height: 60,
+                          child: ReactiveFormConsumer(
+                            builder: (context, form, child) {
+                              return PrimaryButton(
+                                text: 'Send Link',
+                                onPressed: form.valid
+                                    ? () {
+                                        final value = form.value;
+                                        logger.i('Form Values: $value');
+                                      }
+                                    : null,
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 20),
+
+                      // SIGN UP LINK
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text("Don't have an account? "),
+                          MouseRegion(
+                            cursor: SystemMouseCursors.click,
+                            child: GestureDetector(
+                              onTap: () {
+                                // ação para registro
+                              },
+                              child: Text(
+                                "Sign Up",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  color: AppColors.primary,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 200),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
